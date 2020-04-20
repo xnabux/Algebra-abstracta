851f7d8d556892544199f586e3663738bfc6fc1b
joaquin Esteban Cutipa Mamani <63615427+xnabux@users.noreply.github.com>
2020-04-20 15:12:18 -0500

    3e

---
+++ VIGERNE/vigerne_Enca.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class vigerne_2{
+public:
+    vigerne_2();
+    string insertar(string);
+    string cifrar(string);
+    string descifrar(string);
+    string alfabeto;
+
+
+private:
+    string clave;
+
+};
+
